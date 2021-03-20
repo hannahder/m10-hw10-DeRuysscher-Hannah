f3dca97ad6cef4987be19c9ebf920b5df4ca1644
Hannah De Ruysscher <hderuysscher@ufl.edu>
2021-03-20 13:37:25 -0400

    Added ES6 syntax

--- src/main.js
+++ src/main.js
@@ -11,41 +11,39 @@ const y = () => document.getElementById("freezing-temp").innerHTML = 32;
 setTimeout(y, 0);
 
 //Water Temp
-let z = () => document.getElementById("water-temp").innerHTML = '400';
-
-setTimeout(z, 0);
+const water_temp = 415
+document.getElementById("water-temp").innerHTML = water_temp;
 
 
 //After 3000 milliseconds
 
-let a = document.getElementById("water-temp");
-let b = document.getElementById("boiling");
-let c = document.getElementById("frozen");
-let d = document.getElementById("good-temp");
 
-setTimeout(function coldEnough() {
-    if (Number(a.innerHTML) > 212) {
-      b.classList.remove("hide");
-    } else {
-    }
-}, 3000);
+let hotEnough = () => {
+  let waterTemp = document.getElementById("water-temp");
+  let boilingTemp = document.getElementById("boiling");
+  if (Number(waterTemp.innerHTML) > 212) {
+    boilingTemp.classList.remove("hide");
+  } else {
+  }
+}
+setTimeout(hotEnough, 3000);
 
+let coldEnough = () => {
+  let waterTemp = document.getElementById("water-temp");
+  let freezingTemp = document.getElementById("frozen");
+  if (Number(waterTemp.innerHTML) < 32) {
+    freezingTemp.classList.remove("hide");
+  } else {
+  }
+}
+setTimeout(coldEnough, 3000);
 
-setTimeout(function coldEnough() {
-    if (Number(a.innerHTML) < 32) {
-      c.classList.remove("hide");
-    } else {
-    }
-}, 3000);
-
-
-setTimeout(function goodEnough() {
-    if (Number(a.innerHTML) >= 33 && Number(a.innerHTML) <= 211) {
-      d.classList.remove("hide");
-    } else {
-    }
-}, 3000);
-
-hotEnough();
-coldEnough();
-goodEnough();
+let goodEnough = () => {
+  let waterTemp = document.getElementById("water-temp");
+  let goodTemp = document.getElementById("good-temp");
+  if (Number(waterTemp.innerHTML) >= 33 && Number(waterTemp.innerHTML) <= 211) {
+    goodTemp.classList.remove("hide");
+  } else {
+  }
+}
+setTimeout(goodEnough, 3000);
