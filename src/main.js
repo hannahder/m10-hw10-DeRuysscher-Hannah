79317fd1e6b54465fbf88664da9e9844f085115b
Hannah De Ruysscher <hderuysscher@ufl.edu>
2021-03-20 08:22:49 -0400

    Completed 1st part, started 2nd part

--- src/main.js
+++ src/main.js
@@ -1,1 +1,44 @@
 // It is always helpful to use comments in your code!
+
+//Boiling Temp
+const x = () => document.getElementById("boiling-temp").innerHTML = '212';
+
+setTimeout(x, 0);
+
+//Freezing Temp
+const y = () => document.getElementById("freezing-temp").innerHTML = '32';
+
+setTimeout(y, 0);
+
+//Water Temp
+let z = () => document.getElementById("water-temp").innerHTML = '12';
+
+setTimeout(z, 0);
+
+
+//After 3000 milliseconds
+
+let a = document.getElementById("water-temp");
+let b = document.getElementById("boiling");
+
+setTimeout(function hotEnough() {
+    if (a > '212') {
+      b.classList.remove("hide");
+    } else {
+    }
+}, 3000);
+
+hotEnough();
+
+
+
+// const c = document.getElementById("frozen");
+//
+// function coldEnough() {
+//     if (a < '32') {
+//       c.classList.remove("hide");
+//     } else {
+//     }
+// };
+//
+// coldEnough();
